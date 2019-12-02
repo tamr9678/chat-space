48e972e3a0b213e38e374c5df9919bd5c9a2ca52
tamr9678 <tamr9678@gmail.com>
2019-12-02 11:52:22 +0900

    fix_users_route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   # get 'messages/index'
   root to:"messages#index"
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update]
 end
 s
