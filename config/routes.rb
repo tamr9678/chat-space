bb9dd8e08683c2fd63e3e85717c9bebe1362c1c2
tamr9678 <tamr9678@gmail.com>
2019-12-02 13:28:20 +0900

    change _root_to_groups#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   # get 'messages/index'
-  root to:"messages#index"
+  root to:"groups#index"
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update]
 end
