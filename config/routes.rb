d77bff06894dba5c9a230bf6a3f60e4ab035d813
tamr9678 <tamr9678@gmail.com>
2019-12-02 12:19:31 +0900

    set_view_files

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   # get 'messages/index'
   root to:"messages#index"
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update]
 end
