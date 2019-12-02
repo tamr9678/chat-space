1904f5cc77af0ddabcaa1436d9d05374b8d6632c
tamr9678 <tamr9678@gmail.com>
2019-12-02 14:32:05 +0900

    implement_edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,9 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+
+  def index
+  end
+
   def new
     @group = Group.new
     @group.users << current_user
@@ -13,8 +18,24 @@ class GroupsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @group.update(group_params)
+      redirect_to root_path, notice: '更新されました'
+    else
+      render :edit
+    end
+  end
+
   private
   def group_params
     params.require(:group).permit(:name, user_ids: [])
   end
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
 end
