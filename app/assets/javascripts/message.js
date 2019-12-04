8f8cd64bce4e81dd18004882d07a6cb4d5202bd8
tamr9678 <tamr9678@gmail.com>
2019-12-04 12:06:44 +0900

    make_html_in_JS

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,44 @@
 $(function(){
+  function buildHTML(message){
+    // 「もしメッセージに画像が含まれていたら」という条件式
+    if (message.image) {
+      var html = `
+                  <div class= "message">
+                    <div class = message-info>
+                      <div class = message-info_name>
+                        <%= message.user.name %>
+                      </div>
+                      <div class = mesage-info__date>
+                        <%= message.created_at.strftime("%Y/%m/%d %H:%M")
+                      </div>
+                    </div>
+                    <div class = "contents__body">
+                      <%= message.body %>
+                    </div>
+                    <div clas = "lower-message__image">
+                      <%= image_tag message.image.url %>
+                    </div>
+                  </div>  
+                  `
+    } else {
+      var html =  ` 
+                    <div class= "message">
+                      <div class = message-info>
+                        <div class = message-info_name>
+                          <%= message.user.name %>
+                        </div>
+                        <div class = mesage-info__date>
+                          <%= message.created_at.strftime("%Y/%m/%d %H:%M")
+                        </div>
+                      </div>
+                      <div class = "contents__body">
+                        <%= message.body %>
+                      </div>
+                    </div>  
+                  `
+    }
+    return html
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -11,6 +51,14 @@ $(function(){
       processData: false,
       contentType: false
     })
-    console.log(this)
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.comments').append(html);
+      $('.textbox').val('');
+      $('.form__submit').prop('disabled', false);
+    })
+    .fail(function(){
+      alert('error');
+    })
   })
 })
