0ced5d75d4cdfb718535a251407d91a606640ebf
tamr9678 <tamr9678@gmail.com>
2019-12-05 16:25:45 +0900

    add_reloadMessages_to_message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,41 +1,59 @@
 $(function(){
-  function buildHTML(message){
-    if (message.image.url !=  null) {
-      var html = `<div class="message">
-                    <div class="message-info">
-                      <div class="message-info__name">
-                        ${message.user_name}
-                      </div>
-                      <div class="message-info__date">
-                        ${message.created_at}
-                      </div>
-                    </div>
-                    <div class="contents__body">
-                      <p class="message_text">
-                        ${message.body}
-                      </p>
-                      <img class="lower-message__image" src=${message.image.url} >
-                    </div>
-                  </div>`     
-    } else {
-      var html = `<div class="message">
-                    <div class="message-info">
-                      <div class="message-info__name">
-                        ${message.user_name}
-                      </div>
-                      <div class="message-info__date">
-                        ${message.created_at}
-                      </div>
-                    </div>
-                    <div class="contents__body">
-                      <p class="message_text">
-                        ${message.body}
-                      </p>
-                    </div>
-                  </div>`
-    }
-    return html
-  }
+  var buildHTML = function(message) {
+    if (message.body && message.image) {
+      //data-idが反映されるようにしている
+      var html = `<div class="message" data-message_id=` + message.id + `>` +
+        `<div class="message-info">` +
+          `<div class="message-info__name">` +
+            message.user_name +
+          `</div>` +
+          `<div class="message-info__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="lower-message">` +
+          `<p class="lower-message__content">` +
+            message.body +
+          `</p>` +
+          `<img src="` + message.image + `" class="lower-message__image" >` +
+        `</div>` +
+      `</div>`
+    } else if (message.body) {
+      //同様に、data-idが反映されるようにしている
+      var html = `<div class="message" data-message_id=` + message.id + `>` +
+        `<div class="message-info">` +
+          `<div class="message-info__name">` +
+            message.user_name +
+          `</div>` +
+          `<div class="message-info__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="lower-message">` +
+          `<p class="lower-message__content">` +
+            message.body +
+          `</p>` +
+        `</div>` +
+      `</div>`
+    } else if (message.image) {
+      //同様に、data-idが反映されるようにしている
+      var html = `<div class="message" data-message_id=` + message.id + `>` +
+        `<div class="message-info">` +
+          `<div class="message-info__name">` +
+            message.user_name +
+          `</div>` +
+          `<div class="message-info__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="lower-message">` +
+          `<img src="` + message.image + `" class="lower-message__image" >` +
+        `</div>` +
+      `</div>`
+    };
+    return html;
+  };
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -61,4 +79,32 @@ $(function(){
       $('.form-contents__send-btn').removeAttr('disabled');
     });
   })
-})
+  
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id = $('.message:last').data('id');
+    $.ajax({
+      //ルーティングで設定した通りのURLを指定
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      //追加するHTMLの入れ物を作る
+      var insertHTML = '';
+      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      //メッセージが入ったHTMLに、入れ物ごと追加
+      $('.messages').append(insertHTML);
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  setInterval(reloadMessages, 7000);
+});
